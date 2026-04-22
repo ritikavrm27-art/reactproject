3fde03d26943fe46f6e5cc404d56540005c71208
Ritika Verma <ritikavrm27@gmail.com>
2026-04-22 11:57:10 +0530

    added time in db.go

--- Golen_Users/config/db.go
+++ Golen_Users/config/db.go
@@ -13,12 +13,12 @@ var DB *sql.DB
 func ConnectDB() {
 
 //connString := "postgres://postgres:password@localhost:5432/databasename?sslmode=disable"
-	connString := "postgres://postgres:postgres@blog-db:5432/users_golen?sslmode=disable"
+	connString := "postgres://postgres:postgres@user-db:5432/users_golen?sslmode=disable"
 
 	var err error
 
 	// Retry logic
-	for i := 0; i < 10; i++ {
+	for i := 0; i < 20; i++ {
 		DB, err = sql.Open("postgres", connString)
 		if err != nil {
 			log.Println("Error opening DB:", err)
@@ -31,7 +31,7 @@ func ConnectDB() {
 			log.Println("DB not ready yet, retrying...")
 		}
 
-		time.Sleep(2 * time.Second)
+		time.Sleep(3 * time.Second)
 	}
 
 	log.Fatal("Could not connect to DB after multiple attempts")
